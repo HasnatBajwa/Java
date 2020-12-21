37e136d9490948cf67f64649167aa9b15ca550a7
HasnatBajwa <hasnatjbajwa@gmail.com>
2020-12-21 23:53:15 +0500

    Added program03

---
+++ script/program03.js
@@ -0,0 +1,8 @@
+var sub1=parseInt(prompt("Enter Marks of Subject 1:"));
+var sub2=parseInt(prompt("Enter Marks of Subject2:"));
+var sub3=parseInt(prompt("Enter Marks of Subject3:"));
+var average=(sub1+sub2+sub3)/3
+if(average>80)
+{
+    console.log("You are above standard, Admission Granted");
+}
